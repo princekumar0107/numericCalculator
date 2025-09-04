557ae938b2691856bca0bd9dfcf242afb940fbd2
Prince Kumar <pksu420@gmail.com>
2025-09-04 18:34:59 +0530

    Numberic Calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+let input=document.getElementById('in')
+
+let buttons = document.querySelectorAll('button');
+
+let string= " "; // jisme ham apna result store karenge
+ let arr =Array.from(buttons); // all button ka array bna lenge
+ arr.forEach(button => { // for each loop chalayenge for array
+    button.addEventListener('click', (e) =>{ //  jab bhi koi button par click krega to kya perform krega 
+                                        // jab bhi hu button par click krenge to "e" pass hoga
+                                        // => (arrow function means) ki jab bhi hum button par click kre to kya ho.
+        if(e.target.innerHTML == '='){ // agar equal to eval function chala denge
+            string =eval(string) //eval maens evaluate string ko mathemetically convert karta hai
+            input.value=string;
+        }
+        else if(e.target.innerHTML== "AC"){
+            string="";
+            input.value=string;
+        }
+
+        else if(e.target.innerHTML=="DEL"){
+            string = string.substring(0, string.length-1)
+            input.value = string;
+        }
+            
+        else{
+            string+=e.target.innerHTML;
+            input.value = string;
+        }
+
+        
+    
+    })
+ })
